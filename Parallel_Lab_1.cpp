9b6d432977be3d33d2c6a61534a0ea851b41c480
Rainbrand <26818172+Rainbrand@users.noreply.github.com>
2019-10-26 08:41:27 +0500

    Refactored

--- Parallel_Lab_1.cpp
+++ Parallel_Lab_1.cpp
@@ -1,7 +1,4 @@
-﻿// Parallel_Lab_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <thread>
 #include <chrono>
 
@@ -9,7 +6,7 @@ void FirstThread(std::string &message)
 {
 	std::cout << "Thread ID\t" << std::this_thread::get_id() << "\tThis it FirstThread\t" << std::endl;
 	message = "Hello, SecondThread!";
-	std::cout << "Message is changed" << std::endl;
+	std::cout << "Message changed" << std::endl;
 	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 	std::cout << "FirstThread has closed" << std::endl;
 }
@@ -18,7 +15,7 @@ void SecondThread(const std::string &message)
 {
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	std::cout << "Thread ID\t" << std::this_thread::get_id() << "\tThis it SecondThread\t" << std::endl;
-	while(true)
+	while(true)		//Busy waiting
 	{
 		if (!message.empty())
 		{
